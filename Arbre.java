7c9033068140bdc9425c8670cd50710a1ef833b6
ML44 <Matthieu@pc3.home>
2015-01-03 22:01:51 +0100

    Revert bc84711..4d04f87
    This rolls back to commit bc84711302abf12a799571e5e6d3fda1c574a3bd.

--- Arbre.java
+++ Arbre.java
@@ -1,7 +1,4 @@
-
 import java.math.BigInteger;
-import java.util.LinkedList;
-import java.util.List;
 
 public class Arbre {
 	public BigInteger produit;
@@ -9,29 +6,25 @@ public class Arbre {
 	public Arbre fils_gauche;
 	public Arbre fils_droit;
 	
-	public Arbre() //Initialisation vide
-	{
+	public Arbre() {
 		produit = new BigInteger("0");
 		reste = new BigInteger("0");
 		fils_gauche = null;
 		fils_droit = null;
 	}
 	
-	public Arbre(Arbre fg, Arbre fd) //Initialisation avec les arbres fils
-	{
+	public Arbre(Arbre fg, Arbre fd) {
 		produit = new BigInteger("0");
 		reste = new BigInteger("0");
 		fils_gauche = fg;
 		fils_droit = fd;
 	}
 	
-	boolean isLeaf() 
-	{
+	boolean isLeaf() {
 		return ((this.fils_droit==null)&&(this.fils_gauche==null));
 	}
 		
-	BigInteger produitFils() //Calcule le produit des deux noeuds fils (s'ils existent)
-	{
+	BigInteger produitFils() {
 		if(!this.isLeaf())
 		{
 			if(this.fils_droit == null)
@@ -50,12 +43,11 @@ public class Arbre {
 		return null;
 	}
 	
-	void remplaceProduitFils() //Remplace la valeur du noeud produit par le produit des fils
-	{
-		produit = this.produitFils();
+	void remplaceProduitFils() {
+		this.produit = this.produitFils();
 	}
 	
-	 private void print(int k)   // N'est appelé que par print()
+	void print(int k)
 	{
 		if(this!=null)
 		{
@@ -68,9 +60,7 @@ public class Arbre {
 					i=i-1;
 				}
 				System.out.print(" ");
-				System.out.println(this.produit.toString()+ " " + reste.toString());	
-				
-				
+				System.out.println(this.produit.toString());			
 			}
 			else
 			{
@@ -89,120 +79,9 @@ public class Arbre {
 		}
 	}
 	
-	void print() //Affiche l'arbre
+	void print() 
 	{
 		this.print(0);
 	}
 	
-	void completeArbreReste()
-	//Calcule les restes de l'arbre
-	{
-		reste=produit.mod(produit.multiply(produit));
-		if (!(fils_droit==null)){
-			fils_droit.completeArbreReste(reste);
-			}
-			if (!(fils_gauche==null)){
-			fils_gauche.completeArbreReste(reste);
-			}
-		
-	}
-	private void completeArbreReste(BigInteger m)
-	//Calcule les restes d'un sous arbre (modulo m)
-	//N'est appelé que par completeAbreReste()
-	{          
-		reste=m.mod(produit.multiply(produit));
-		if (!(fils_droit==null)){
-		fils_droit.completeArbreReste(reste);
-		}
-		if (!(fils_gauche==null)){
-		fils_gauche.completeArbreReste(reste);
-		}
-		
-	}
-	
-	private static Arbre construitArbreProduit(List<BigInteger> liste, int a, int b)
-	//Construit l'arbre des produits des restes indexés de a à b dans la liste
-	//N'est appelé que par construitAtbreProduit(List<BigInteger>)
-	{
-		Arbre mon_arbre;
-		if(a==b)
-		{
-			mon_arbre = new Arbre();
-			mon_arbre.produit = liste.get(a);
-		}
-		else
-		{
-			int m = (b+a)/2; //?
-			Arbre mon_arbre_gauche = construitArbreProduit(liste, a, m);
-			Arbre mon_arbre_droit = construitArbreProduit(liste, m+1, b);
-			mon_arbre = new Arbre(mon_arbre_gauche, mon_arbre_droit);
-			mon_arbre.remplaceProduitFils();
-		}
-
-		return mon_arbre;
-	}
-	
-	public static Arbre construitArbreProduit(List<BigInteger> liste)
-	//Construit l'arbre des produits de toute la liste
-	{
-		int n = liste.size();
-		return construitArbreProduit(liste, 0, n-1);//start à 0 ou 1 ?
-	}
-	
-	public Arbre construitArbreVide(int n)
-	//Construit arbre vide à n feuilles
-	{
-		if(n==0)
-		{
-			return null;
-		}
-		if(n==1)
-		{
-			return new Arbre();
-		}
-		else
-		{
-			int b = n/2;
-			int a = n - b;
-			return new Arbre(construitArbreVide(a),construitArbreVide(b));
-		}		
-	}
-	
-	private void recupereFacteursPremiers(List<DoubleInt> L)
-	//Explore l'arbre en profondeur et ajoute à L les pgcd non triviaux des feuilles
-	//Appelée par recupereFacteursPremiers()
-	{
-		
-		if(this.isLeaf())
-		{
-			BigInteger facteur=Main.gcd(reste.divide(produit),produit);
-			
-			if(facteur.equals(BigInteger.ONE)==false)
-			{
-			L.add( new DoubleInt(facteur, produit));
-			// Tu stockes le facteur ainsi que la clé ?
-			}
-		}
-		else 
-		{
-			if(!(fils_droit==null)) 
-			{
-				fils_droit.recupereFacteursPremiers(L);
-			}
-			if(!(fils_gauche==null)) 
-			{
-				fils_gauche.recupereFacteursPremiers(L);
-			}
-		}		
-	}
-	
-	public List<DoubleInt> recupereFacteursPremiers() 
-	//Explore en profondeur et renvoie une liste des pgcd non triviaux des feuilles
-	{
-		List<DoubleInt> ans=new LinkedList<DoubleInt>();
-		recupereFacteursPremiers(ans);
-		return ans;
-		}
-		
-	}
-
+}
--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 
+
 import java.util.ArrayList;
 import java.io.FileReader;
 import java.io.BufferedReader;
@@ -10,9 +11,7 @@ import java.math.BigInteger;
 	
 public class Main {
 	  
-	  static BigInteger gcd(BigInteger x,BigInteger y) 
-	  //PGCD de 2 BigInteger (par soustraction)
-	  {
+	  static BigInteger gcd(BigInteger x,BigInteger y) {
 			BigInteger r0 = x, r1 = x, r2 = y;
 			
 			while(!(r2.equals(new BigInteger("0"))))
@@ -24,23 +23,17 @@ public class Main {
 			return r1;
 		}
 	  
-	  static List<BigInteger> textToList(String nom) throws IOException 
-	  //Transformer le fichier .txt en liste de BigInteger
-	  {
+	  static List<BigInteger> textToList(String nom) throws IOException {
 			BufferedReader in = new BufferedReader(new FileReader(""+nom));
 			String line;
 			String nombre;
 			List<BigInteger> liste = new ArrayList<BigInteger>();
-			//while ((line = in.readLine()) != null)  
-			//Pourquoi ca marche pas ça ?
-			//Ca marche mais c'était pour le test sur les 500 premières valeurs que j'ai besoin de la 2e forme
+			//while ((line = in.readLine()) != null)
 			for(int i = 1;i<1000;i++)
 			{
 				line=in.readLine();
 				nombre = "";
-				for(int k =16;k<line.length();k++)  
-					// Pourquoi 17 ?
-					// C'est le nombre de caractères avant le début du nombre
+				for(int k =17;k<line.length();k++)
 				{
 					nombre = nombre + line.charAt(k);
 				}
@@ -52,27 +45,96 @@ public class Main {
 			return liste;
 	  }
 
+	public Arbre construitArbreVide(int n) {
+		if(n==0)
+		{
+			return null;
+		}
+		if(n==1)
+		{
+			return new Arbre();
+		}
+		else
+		{
+			int b = n/2;
+			int a = n - b;
+			return new Arbre(construitArbreVide(a),construitArbreVide(b));
+		}		
+	}
 	
-//*********************************************************************	
-	 
+	public static Arbre construitArbreProduit(List<BigInteger> liste, int a, int b)
+	{
+		Arbre mon_arbre;
+		if(a==b)
+		{
+			mon_arbre = new Arbre();
+			mon_arbre.produit = liste.get(a);
+		}
+		else
+		{
+			int m = (b+a)/2; //?
+			Arbre mon_arbre_gauche = construitArbreProduit(liste, a, m);
+			Arbre mon_arbre_droit = construitArbreProduit(liste, m+1, b);
+			mon_arbre = new Arbre(mon_arbre_gauche, mon_arbre_droit);
+			mon_arbre.remplaceProduitFils();
+		}
+
+		return mon_arbre;
+	}
+	
+	public static Arbre construitArbreProduit(List<BigInteger> liste)
+	{
+		int n = liste.size();
+		return construitArbreProduit(liste, 0, n-1);//start à 0 ou 1 ?
+	}
+	
 		
 	public static void main(String[] args)  throws IOException {
+/*
+		long tStart = System.currentTimeMillis();
 
-		//long tStart = System.currentTimeMillis();
-
-		List<BigInteger> liste = textToList("keys/keys100.txt");
-		System.out.print(liste.toString());
-		
-		
-		/*
-		mon_arbre.completeArbreReste();
+		List<BigInteger> liste = textToList("keys/keys1000.txt");
+		for(int i=0; i+1<liste.size(); i++)
+		{
+			BigInteger a = liste.get(i);
+			
+			for(int j=i+1; j<liste.size(); j++)
+			{
+				BigInteger b = liste.get(j);
+				BigInteger d = gcd(a,b);
+				if(d.toString().length()>5)
+				{
+					System.out.println("--------------");
+					System.out.println("PGCD(key"+(i+1)+",key"+(j+1)+") = ");
+					System.out.println(d.toString());
+					System.out.println("--------------");
+				}
+				
+			}
+		}
 		long tEnd = System.currentTimeMillis();
 		long tDelta = tEnd - tStart;
+		System.out.print(tDelta);
+	*/	
 		
-		System.out.println(tDelta);
+	/*	
+		Arbre mon_arbre1 = new Arbre();
+		mon_arbre1.produit = new BigInteger("3");
+		Arbre mon_arbre2 = new Arbre();
+		mon_arbre2.produit = new BigInteger("2");
+		Arbre mon_arbre = new Arbre(mon_arbre1,mon_arbre2);
+		mon_arbre.remplaceProduitFils();
+	*/
+		List<BigInteger> ma_liste = new ArrayList<BigInteger>();
+		ma_liste.add(new BigInteger("3"));
+		ma_liste.add(new BigInteger("2"));
+		ma_liste.add(new BigInteger("2"));
+		ma_liste.add(new BigInteger("2"));
+
+
+		Arbre mon_arbre = construitArbreProduit(ma_liste);
+		mon_arbre.print();
 		
-		System.out.println(mon_arbre.recupereFacteursPremiers().get(5));
-		*/
 	}
 	
 		
